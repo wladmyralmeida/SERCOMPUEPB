ecbcd52803b1e4adc2cdec8292e5cdf04615ecbf
Wladmyr Almeida <Wladmyr.s.squadp@genial.com.vc>
2025-03-17 20:32:52 -0300

    Comecei o projeto do 0

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/app/home/home_page.dart';
-import 'package:todoapp/core/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isDarkMode = true; // ✅ Começa no tema escuro
-
-  void toggleTheme() {
-    setState(() {
-      isDarkMode = !isDarkMode;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meu Supermercado',
-      debugShowCheckedModeBanner: false,
-      theme: isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme,
-      home: HomePage(title: 'Tauste Supermarket', onToggleTheme: toggleTheme),
-    );
+    return MaterialApp();
   }
 }
